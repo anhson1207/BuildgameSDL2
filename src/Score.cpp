e4dd395dc4991a7286fb0b8160d60b03389ea29e
anhson1207 <anhsondoraemon@gmail.com>
2025-03-07 21:18:08 +0700

    score

---
+++ src/Score.cpp
@@ -0,0 +1,8 @@
+//
+//  Score.cpp
+//  SDL2
+//
+//  Created by Nguyễn Anh Sơn on 07/03/2025.
+//
+
+#include "Score.hpp"
---
+++ src/Score.hpp
@@ -0,0 +1,13 @@
+//
+//  Score.hpp
+//  SDL2
+//
+//  Created by Nguyễn Anh Sơn on 07/03/2025.
+//
+
+#ifndef Score_hpp
+#define Score_hpp
+
+#include <stdio.h>
+
+#endif /* Score_hpp */
